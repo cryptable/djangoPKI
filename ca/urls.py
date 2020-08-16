3338d24342c3c4446342eeee622ae7c8f0aeab51
David Tillemans <davidtillemans@cryptable.org>
2020-08-16 09:22:13 +0200

    certy p10: needs refactoring

--- ca/urls.py
+++ ca/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'ca'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.CertificatesOfCAView.as_view(), name='certs_of_ca')
+    path('<int:pk>/', views.CertificatesOfCAView.as_view(), name='certs_of_ca'),
+    path('<int:ca_id>/fillin_p10', views.fillin_p10, name='fillin_p10'),
+    path('<int:ca_id>/certify_p10', views.certify_p10, name='certify_p10'),
 ]
 
--- ca/views.py
+++ ca/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.views import generic
+from django.urls import reverse
 
 from .models import CA, Certificate
 
@@ -17,11 +20,23 @@ class CertificatesOfCAView(generic.ListView):
     template_name = 'ca/certs_of_ca.html'
 
     def get_context_data(self, **kwargs):
-        subject = self.object_list[0].subject_text
+        ca_cert = self.object_list[0]
         context = Certificate.objects\
-            .filter(issuer_text__exact=subject)\
-            .exclude(subject_text__exact=subject)
+            .filter(issuer_text__exact=ca_cert.subject_text)\
+            .exclude(subject_text__exact=ca_cert.subject_text)
         return {
-            'ca_name': subject,
+            'ca': ca_cert,
             'certificate_list': context,
         }
+
+
+def fillin_p10(request, ca_id):
+    ca_cert = get_object_or_404(Certificate, pk=ca_id)
+    return render(request, 'ca/fillin_p10.html', {
+        'ca':ca_cert })
+
+
+def certify_p10(request, ca_id):
+    ca_cert = get_object_or_404(Certificate, pk=ca_id)
+    p10 = request.POST['p10']
+    return HttpResponseRedirect(reverse('ca:certs_of_ca', args=(ca_id,)))
